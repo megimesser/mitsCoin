1fa4e8461817ed05fe2d8e52c4cda41d29411e61
megimesser <heinze.obach@web.de>
2024-04-10 16:16:41 +0200

    gl

--- project1/mitscoin_v02/src/components/head.jsx
+++ project1/mitscoin_v02/src/components/head.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 
-
 function Head() {
+  // Definiere die Funktion twitter innerhalb der Head-Komponente
+  const twitter = () => {
+    window.open("https://twitter.com/mitsmeme1?s=21");
+  };
+
+  const telegram = () => {
+    window.open("https://t.me/MITSMeme1");
+  };
+
+  const dexscrener = () => {
+    window.open("https://dexscreener.com/solana/fhyt96zzh5qcgzuggaz17egenpe8mbkkjwoccfztwssu");
+  };
+
+
+
   return (
     <div>
       <header className="heading">
-        <img src="./bilder/log2.jpg"></img>
+        <img src="./bilder/log2.jpg" alt="Logo"></img>
         <ul>
-          <li>Telegram</li>
-          <li>Twitter (X)</li>
-          <li>DEXSCRENER</li>
+          {/* Übergebe die Funktion twitter als Prop */}
+          <li onClick={telegram}>Telegram</li>
+          <li onClick={twitter}>Twitter (X)</li>
+          <li onClick={dexscrener}>DEXSCREENER</li>
           <li>About us</li>
         </ul>
       </header>
